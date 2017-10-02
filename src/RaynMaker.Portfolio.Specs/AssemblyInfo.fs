dd54f644ca832091b4bdfaef7b46f8bf9be699fc
bg0jr <bg0jr@shared-files.de>
2017-10-02 06:44:03 +0200

    core and test project added

--- src/RaynMaker.Portfolio.Specs/AssemblyInfo.fs
+++ src/RaynMaker.Portfolio.Specs/AssemblyInfo.fs
@@ -1,16 +1,15 @@
-﻿namespace Portfolio.AssemblyInfo
+﻿namespace RaynMaker.Portfolio.Specs.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyTitle("RaynMaker.Portfolio.Web")>]
+[<assembly: AssemblyTitle("RaynMaker.Portfolio.Specs")>]
 [<assembly: AssemblyProduct("RaynMaker")>]
 [<assembly: AssemblyCopyright("Copyright © Licence Owner 2017")>]
 
 [<assembly: ComVisible(false)>]
-
-[<assembly: Guid("343e3582-f5d1-4a99-922a-a8129b4e19fd")>]
+[<assembly: Guid("94678ac4-8845-4453-b244-e13b62dac0d4")>]
 
 [<assembly: AssemblyVersion("1.0.0.0")>]
 
--- src/RaynMaker.Portfolio/AssemblyInfo.fs
+++ src/RaynMaker.Portfolio/AssemblyInfo.fs
@@ -1,16 +1,15 @@
-﻿namespace Portfolio.AssemblyInfo
+﻿namespace RaynMaker.Portfolio.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
 
-[<assembly: AssemblyTitle("RaynMaker.Portfolio.Web")>]
+[<assembly: AssemblyTitle("RaynMaker.Portfolio")>]
 [<assembly: AssemblyProduct("RaynMaker")>]
 [<assembly: AssemblyCopyright("Copyright © Licence Owner 2017")>]
 
 [<assembly: ComVisible(false)>]
-
-[<assembly: Guid("343e3582-f5d1-4a99-922a-a8129b4e19fd")>]
+[<assembly: Guid("fb545ea0-6567-4392-9d68-b19eb27b84db")>]
 
 [<assembly: AssemblyVersion("1.0.0.0")>]
 
