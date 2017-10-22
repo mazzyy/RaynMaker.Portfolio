60802df3450834c5d3aa55cf24e53c2ce42723fa
bg0jr <bg0jr@shared-files.de>
2017-10-22 18:25:44 +0200

    vue project prepared for migrating current UI to it

--- src/RaynMaker.Portfolio.Html/src/App.vue
+++ src/RaynMaker.Portfolio.Html/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png"></img>
     <router-view/>
+      <div>
+      </div>
   </div>
 </template>
 
@@ -11,13 +13,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
