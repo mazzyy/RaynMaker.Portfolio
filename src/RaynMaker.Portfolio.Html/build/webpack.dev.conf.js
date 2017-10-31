3dc95bea3c549e4c2490b4229be10c026b90ef3d
bg0jr <bg0jr@shared-files.de>
2017-10-31 13:35:26 +0100

    webpack doc improved

--- src/RaynMaker.Portfolio.Html/build/webpack.dev.conf.js
+++ src/RaynMaker.Portfolio.Html/build/webpack.dev.conf.js
@@ -17,6 +17,7 @@ module.exports = merge(baseWebpackConfig, {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // cheap-module-eval-source-map is faster for development
+  // change to "#source-map" to be able to set break points
   devtool: '#cheap-module-eval-source-map',
   plugins: [
     new webpack.DefinePlugin({
