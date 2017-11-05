336c4d4d61e09672ee2ae6f088be0f459e3d403f
bg0jr <bg0jr@shared-files.de>
2017-11-05 10:46:12 +0100

    tests added

---
+++ src/RaynMaker.Portfolio.Specs/EntitiesSpecs.fs
@@ -0,0 +1,48 @@
+﻿namespace RaynMaker.Portfolio.Specs
+
+open System
+open NUnit.Framework
+open FsUnit
+open RaynMaker.Portfolio.Entities
+
+[<TestFixture>]
+module ``Given a sequence of events`` =
+    [<Test>]
+    let ``<When> empty <Then> not last price can be found``() =
+        [] 
+        |> Events.LastPriceOf <| isin "Any Inc"
+        |> should equal None
+
+    [<Test>]
+    let ``<When> last event is a 'buy' <Then> price is taken from that``() =
+        [
+            at 2016 10 10 |> buy "Joe Inc" 10 10.0
+            at 2016 11 10 |> sell "Joe Inc" 5 11.0
+            at 2016 12 10 |> buy "Joe Inc" 5 12.0
+            at 2016 12 12 |> buy "Jacky Limited" 15 200.0
+        ] 
+        |> Events.LastPriceOf <| isin "Joe Inc"
+        |> should equal ({ Day = at 2016 12 10; Value = 12.0M<Currency> } |> Some)
+
+    [<Test>]
+    let ``<When> last event is a 'sell' <Then> price is taken from that``() =
+        [
+            at 2016 10 10 |> buy "Joe Inc" 10 10.0
+            at 2016 11 10 |> sell "Joe Inc" 5 11.0
+            at 2016 12 12 |> buy "Jacky Limited" 15 200.0
+        ] 
+        |> Events.LastPriceOf <| isin "Joe Inc"
+        |> should equal ({ Day = at 2016 11 10; Value = 11.0M<Currency> } |> Some)
+
+
+    [<Test>]
+    let ``<When> last event is a 'priced' <Then> price is taken from that``() =
+        [
+            at 2016 10 10 |> buy "Joe Inc" 10 10.0
+            at 2016 11 10 |> sell "Joe Inc" 5 11.0
+            at 2016 12 11 |> price "Joe Inc" 14.0
+            at 2016 12 12 |> buy "Jacky Limited" 15 200.0
+        ] 
+        |> Events.LastPriceOf <| isin "Joe Inc"
+        |> should equal ({ Day = at 2016 12 11; Value = 14.0M<Currency> } |> Some)
+
---
+++ src/RaynMaker.Portfolio.Specs/FakeBroker.fs
@@ -0,0 +1,39 @@
+﻿namespace RaynMaker.Portfolio.Specs
+
+open System
+open RaynMaker.Portfolio.Entities
+
+[<AutoOpen>]
+module internal FakeBroker = 
+    let fee = 9.9M<Currency> 
+
+    let at year month day = new DateTime(year, month, day)
+
+    let isin company = company.GetHashCode() |> sprintf "US%i" |> Isin
+
+    let buy company count (price:float) date =
+        { StockBought.Date = date 
+          Name = company
+          Isin = company |> isin
+          Count = count |> decimal
+          Price = 1.0M<Currency> * (price |> decimal)
+          Fee = fee
+        } |> StockBought
+
+    let sell company count (price:float) date =
+        { StockSold.Date = date 
+          Name = company
+          Isin = sprintf "US%i" (company.GetHashCode()) |> Isin
+          Count = count |> decimal
+          Price = 1.0M<Currency> * (price |> decimal)
+          Fee = fee
+        } |> StockSold
+
+    let price company (price:float) date =
+        { StockPriced.Date = date 
+          Name = company
+          Isin = sprintf "US%i" (company.GetHashCode()) |> Isin
+          Price = 1.0M<Currency> * (price |> decimal)
+        } |> StockPriced
+
+
