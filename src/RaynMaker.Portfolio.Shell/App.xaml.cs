d49fab51c62b7a60321a9658c56a6c39c5fad694
bg0jr <bg0jr@shared-files.de>
2018-10-03 10:37:43 +0200

    last chance exception handler added

--- src/RaynMaker.Portfolio.Shell/App.xaml.cs
+++ src/RaynMaker.Portfolio.Shell/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
+using Plainion;
 using static RaynMaker.Portfolio.Main;
 
 namespace RaynMaker.Portfolio.Shell
@@ -16,11 +18,21 @@ namespace RaynMaker.Portfolio.Shell
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            Debugger.Launch();
+            DispatcherUnhandledException += OnUnhandledException;
+
             var projectFile = RaynMaker.Portfolio.Main.getProjectFileFromCommandLine(Environment.GetCommandLineArgs().Skip(1).ToArray());
             myInstance = RaynMaker.Portfolio.Main.start(projectFile);
             Port = myInstance.port;
 
             base.OnStartup(e);
+        }
+
+        private void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Dump(), "Unhandled exception", MessageBoxButton.OK, MessageBoxImage.Error);
+
+            e.Handled = true;
         }
 
         protected override void OnExit(ExitEventArgs e)
