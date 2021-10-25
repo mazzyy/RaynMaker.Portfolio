fb4094f350d56bb16ffdc6b1f7442ec154a8b87b
bg0jr <bg0jr@shared-files.de>
2021-10-25 13:43:04 +0200

    Build WebUI always for production

--- src/RaynMaker.Portfolio/AssemblyInfo.fs
+++ src/RaynMaker.Portfolio/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("RaynMaker.Portfolio")>]
 [<assembly: AssemblyProductAttribute("RaynMaker.Portfolio")>]
 [<assembly: AssemblyDescriptionAttribute("RaynMaker.Portfolio")>]
-[<assembly: AssemblyCopyrightAttribute("Copyright @ 2020")>]
+[<assembly: AssemblyCopyrightAttribute("Copyright @ 2021")>]
 [<assembly: AssemblyVersionAttribute("1.4")>]
 [<assembly: AssemblyFileVersionAttribute("1.4")>]
 do ()
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "RaynMaker.Portfolio"
     let [<Literal>] AssemblyProduct = "RaynMaker.Portfolio"
     let [<Literal>] AssemblyDescription = "RaynMaker.Portfolio"
-    let [<Literal>] AssemblyCopyright = "Copyright @ 2020"
+    let [<Literal>] AssemblyCopyright = "Copyright @ 2021"
     let [<Literal>] AssemblyVersion = "1.4"
     let [<Literal>] AssemblyFileVersion = "1.4"
